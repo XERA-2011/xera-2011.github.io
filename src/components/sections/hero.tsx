8489b7a4542a02d6c7ce79041f2f330a8514fcbf
🍀 <50286230+XERA-2011@users.noreply.github.com>
2025-12-15 09:10:11 +0800

    feat: 修改文件夹名

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -4,7 +4,7 @@ import { useRef, useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
-import ClockCircle from '@/components/clock/circle';
+import ClockCircle from '@/components/clocks/circle';
 
 export default function HeroSection() {
   const containerRef = useRef<HTMLDivElement>(null);
